6d6112d6e0907314da93da0fac959f92e0cf77e3
futuregit <gmail.com>
2021-10-26 17:30:36 -0400

    Added sample component for test

---
+++ src/pages/ProductSingleRow.js
@@ -0,0 +1,115 @@
+import React from 'react';
+
+function ProductSingleRow() {
+    return (
+    <div class="product product-single row">
+        <div class="col-md-7 mb-5">
+            <div class="product-gallery product-gallery-sticky row cols-xs-2 cols-1">
+                <div class="product-image-wrapper">
+                    <figure class="product-image mb-4" >
+                        <img src="assets/images/products/grid/1-800x900.jpg" data-zoom-image="assets/images/products/grid/1-800x900.jpg" alt="Summer Sport Shoes" width="800" height="900" />
+                        <a href="#" class="product-gallery-btn product-gallery-btn product-image-full"><i class="w-icon-zoom"></i></a>
+                    <img role="presentation" alt="Summer Sport Shoes" src="file:///C:/Users/Family/Downloads/Wolmart%20Sample/Wolmart%20Sample/assets/images/products/grid/1-800x900.jpg" class="zoomImg"  /></figure>
+                </div>
+                <div class="product-image-wrapper">
+                    <figure class="product-image mb-4" st>
+                        <img src="assets/images/products/grid/2-800x900.jpg" data-zoom-image="assets/images/products/grid/2-800x900.jpg" alt="Summer Sport Shoes" width="488" height="549" />
+                        <a href="#" class="product-gallery-btn product-gallery-btn product-image-full"><i class="w-icon-zoom"></i></a>
+                    <img role="presentation" alt="Summer Sport Shoes" src="file:///C:/Users/Family/Downloads/Wolmart%20Sample/Wolmart%20Sample/assets/images/products/grid/2-800x900.jpg" class="zoomImg" /></figure>
+                </div>
+                <div class="product-image-wrapper">
+                    <figure class="product-image mb-4" >
+                        <img src="assets/images/products/grid/3-800x900.jpg" data-zoom-image="assets/images/products/grid/3-800x900.jpg" alt="Summer Sport Shoes" width="800" height="900" />
+                        <a href="#" class="product-gallery-btn product-gallery-btn product-image-full"><i class="w-icon-zoom"></i></a>
+                    <img role="presentation" alt="Summer Sport Shoes" src="file:///C:/Users/Family/Downloads/Wolmart%20Sample/Wolmart%20Sample/assets/images/products/grid/3-800x900.jpg" class="zoomImg"  /></figure>
+                </div>
+                <div class="product-image-wrapper">
+                    <figure class="product-image mb-4" >
+                        <img src="assets/images/products/grid/4-800x900.jpg" data-zoom-image="assets/images/products/grid/4-800x900.jpg" alt="Summer Sport Shoes" width="800" height="900" />
+                        <a href="#" class="product-gallery-btn product-gallery-btn product-image-full"><i class="w-icon-zoom"></i></a>
+                    <img role="presentation" alt="Summer Sport Shoes" src="file:///C:/Users/Family/Downloads/Wolmart%20Sample/Wolmart%20Sample/assets/images/products/grid/4-800x900.jpg" class="zoomImg"  /></figure>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-5 sticky-sidebar-wrapper mb-5">
+            <div class="pin-wrapper" ><div class="product-details sticky-sidebar sticky-sidebar-fixed">
+                <h1 class="product-title">Summer Sport Shoes</h1>
+                <div class="product-bm-wrapper">
+                    <figure class="brand">
+                        <img src="assets/images/products/brand/brand-1.jpg" alt="Brand" width="102" height="48" />
+                    </figure>
+                    <div class="product-meta">
+                        <div class="product-categories">
+                            Category:
+                            <span class="product-category"><a href="#">Sports</a></span>
+                        </div>
+                        <div class="product-sku">
+                            SKU: <span>MS46891344</span>
+                        </div>
+                    </div>
+                </div>
+
+                <hr class="product-divider" />
+
+                <div class="product-price"><ins class="new-price">$86.00</ins></div>
+
+                <div class="ratings-container">
+                    <div class="ratings-full">
+                        <span class="ratings"></span>
+                        <span class="tooltiptext tooltip-top">5.00</span>
+                    </div>
+                    <a href="#product-tab-reviews" class="rating-reviews">(3 Reviews)</a>
+                </div>
+
+                <div class="product-short-desc lh-2">
+                    <ul class="list-type-check list-style-none">
+                        <li>Ultrices eros in cursus turpis massa cursus mattis.</li>
+                        <li>Volutpat ac tincidunt vitae semper quis lectus.</li>
+                        <li>Aliquam id diam maecenas ultricies mi eget mauris.</li>
+                    </ul>
+                </div>
+
+                <hr class="product-divider" />
+
+                <p class="stock">59 in stock</p>
+
+                <div class="sticky-content-wrapper" ><div class="product-sticky-content sticky-content fix-bottom">
+                        <div class="product-form container">
+                            <div class="product product-list-sm mr-auto">                                        <figure class="product-media">                                        <img src="assets/images/products/grid/1-800x900.jpg" alt="Product" width="85" height="85" />                                        </figure>                                        <div class="product-details pt-0 pl-2 pr-2">                                        <h4 class="product-name font-weight-normal mb-1">Summer Sport Shoes</h4>                                        <div class="product-price mb-0">                                        <ins class="new-price">$86.00</ins><del class="old-price"></del></div>                                        </div></div><div class="product-qty-form">
+                                <div class="input-group">
+                                    <input class="quantity form-control" type="number" min="1" max="10000000" />
+                                    <button class="quantity-plus w-icon-plus"></button>
+                                    <button class="quantity-minus w-icon-minus"></button>
+                                </div>
+                            </div>
+                            <button class="btn btn-primary btn-cart">
+                                <i class="w-icon-cart"></i>
+                                <span>Add to Cart</span>
+                            </button>
+                        </div>
+                    </div></div>
+
+                    <div class="social-links-wrapper">
+                        <div class="social-links">
+                            <div class="social-icons social-no-color border-thin">
+                                <a href="#" class="social-icon social-facebook w-icon-facebook"></a>
+                                <a href="#" class="social-icon social-twitter w-icon-twitter"></a>
+                                <a href="#" class="social-icon social-pinterest fab fa-pinterest-p"></a>
+                                <a href="#" class="social-icon social-whatsapp fab fa-whatsapp"></a>
+                                <a href="#" class="social-icon social-youtube fab fa-linkedin-in"></a>
+                            </div>
+                        </div>
+                        <span class="divider d-xs-show"></span>
+                        <div class="product-link-wrapper d-flex">
+                            <a href="#" class="btn-product-icon btn-wishlist w-icon-heart"><span></span></a>
+                            <a href="#" class="btn-product-icon btn-compare btn-icon-left w-icon-compare"><span></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+}
+
+export default ProductSingleRow;
